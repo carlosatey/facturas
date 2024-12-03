ac1c2f5fb0ff69a8e33a20704fd6a487273a90e6
carlosatey <carlosalberto@imaginagroup.com>
2024-12-03 23:09:58 +0100

    Creacion de contexto para react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { ChakraProvider} from '@chakra-ui/react';
 import { createRoot } from 'react-dom/client'
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './index.css'
 import App from './App.tsx'
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <App />
+        <ReactQueryDevtools/>
+      </ChakraProvider>
+    </QueryClientProvider>
 )
