fdf55099c18c0d17e8f1cd6f53464032d2d6c543
carlosalberto <carlosalberto@imaginagroup.com>
2024-12-10 11:12:00 +0100

    Aislamos el componente Select

---
+++ src/components/Select.tsx
@@ -0,0 +1,26 @@
+import { Select as InputSelect} from '@chakra-ui/react';
+
+interface Select {
+    handleFilter: (param: boolean|string) => void
+}
+
+const Select = ({handleFilter}:Select) => {
+
+    return (
+        <InputSelect placeholder='Todas las facturas' onChange={(e) => {
+            const value = e.target.value;
+            if (value === 'paid') {
+                handleFilter(true);
+            } else if (value === 'notPaid') {
+                handleFilter(false);
+            }else {
+                handleFilter('')
+            }
+        }}>
+            <option value='paid' >Pagada</option>
+            <option value='notPaid'>No Pagada</option>
+        </InputSelect>
+    )
+}
+
+export {Select}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,13 @@ import { Table } from "../components/Table";
 import { useFacturas } from "../hooks/useFacturas";
 import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
-import { Button, Flex, Select, Box, Text } from '@chakra-ui/react';
+import { Button, Flex, Box, Text } from '@chakra-ui/react';
 import { useState } from "react";
 import { Facturas } from "../interfaces/Facturas";
 import { Spinner } from "../components/Spinner";
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "../context/auth.context"; 
+import { Select } from "../components/Select";
 
 const Home = () => {
     const [filteredData, setFilteredData] = useState<Facturas[]>([]);
@@ -20,9 +21,13 @@ const Home = () => {
         queryFn: () => getFacturas(),
     })
 
-    const handleFilter = (param:boolean) => {
-        const facturasPagadas = data.filter((factura: Facturas) => factura.paid === param);
-        setFilteredData(facturasPagadas)
+    const handleFilter = (param:boolean|string) => {
+        if( typeof param === "boolean"){
+            const facturasPagadas = data.filter((factura: Facturas) => factura.paid === param);
+            setFilteredData(facturasPagadas)
+        }else{
+            setFilteredData([])
+        }
     }
 
     return (
@@ -40,26 +45,19 @@ const Home = () => {
                 <>
                     <Flex flexDirection='column' width={'100%'} px={20}>
                         <Box w='100%' p={4} color='black' display={"flex"} gap={10}>
-                            <Select placeholder='Todas las facturas' onChange={(e) => {
-                                const value = e.target.value;
-                                if (value === 'paid') {
-                                    handleFilter(true);
-                                } else if (value === 'notPaid') {
-                                    handleFilter(false);
-                                }else {
-                                    setFilteredData([])
-                                }
-                            }}>
-                                <option value='paid' >Pagada</option>
-                                <option value='notPaid'>No Pagada</option>
-                            </Select>
+                            
+                            <Select handleFilter={handleFilter}/>
+                            
                             <Button colorScheme="green" px={8}>
                                 <Link to="/new">Agregar Factura</Link>
                             </Button>
+                            
                             <Button onClick={ ()=> logout(navigate)} px={8}>
                                 Cerrar Sesion
+                            
                             </Button>
                         </Box>
+
                         <Table facturas={filteredData.length ? filteredData: data}/>
                         
                     </Flex>    
